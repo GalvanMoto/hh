1880773e02855a55cc2f27117df705ebe62161f7
Gautam Bhai <59993478+GalvanMoto@users.noreply.github.com>
2025-04-28 22:49:10 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ window.addEventListener('scroll', () => {
 });
 
 // Mobile menu toggle
-const mobileMenuButton = document.querySelector('.md\\:hidden');
-const mobileMenu = document.querySelector('.md\\:flex');
+const mobileMenuButton = document.getElementById('mobileMenuButton');
+const mobileMenu = document.getElementById('mobileMenu');
 
 mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
